b105cc474ff15e71b7a941e7ceb6a8eb8fae4e9e
Hono <suinaixiangaocun@gmail.com>
2025-08-07 01:48:37 +0900

    feat: Todo一覧ページのBladeテンプレートとCSSを作成
    ・レイアウト継承をlayouts.appに修正
    ・更新フォームのHTTPメソッドをPATCHに変更
    ・削除フォームにDELETEメソッド追加
    ・クラス名の誤字修正など細かい調整

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+<div class="todo__alert">
+    <p class="todo__alert--success">
+        Todoを作成しました
+    </p>
+</div>
+
+<div class="todo__content">
+    <form class="create-form" action="/todo/create" method="post">
+        @csrf
+        <div class="create-form__item">
+            <input class="create-form__item-input" type="text" name="content">
+        </div>
+        <div class="create-form__button">
+            <button class="create-form__button-submit">作成</button>
+        </div>
+    </form>
+
+    <div class="todo-table">
+        <table class="todo-table__inner">
+            <tr class="todo--table__row">
+                <th class="todo-table__header" colspan="2">Todo</th>
+            </tr>
+            <tr class="todo-table__row">
+                <td class="todo-table__item">
+                    <form class="update-form" action="/todo/update" method="get">
+                        <div class="update-form__item">
+                            <input class="update-form__item-input" type="text" name="content" value="test">
+                        </div>
+                        <div class="update-form__button">
+                            <button class="update-form__button-submit" type="submit">更新</button>
+                        </div>
+                    </form>
+                </td>
+                <td class="todo-table__item">
+                    <form class="delete-form" action="/todo/delete" method="post">
+                        @csrf
+                        <div class="delete-form__button">
+                            <button class="delete-form__button-submit" type="submit">削除</button>
+                        </div>
+                    </form>
+                </td>
+            </tr>
+            <tr class="todo-table__row">
+                <td class="todo-table__item">
+                    <form class="update-form" action="/todo/update" method="get">
+                        <div class="update-form__item">
+                            <input class="update-form__item-input" type="text" name="content" value="test2">
+                        </div>
+                        <div class="update-form__button">
+                            <button class="update-form__button-submit" type="submit">更新</button>
+                        </div>
+                    </form>
+                </td>
+                <td class="todo-table__item">
+                    <form class="delete-form" action="/todo/delete" method="post">
+                        @csrf
+                        <div class="delete-form__button">
+                            <button class="delete-form__button-submit" type="submit">削除</button>
+                        </div>
+                    </form>
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
+@endsection
